e145fcf4ea49e97351749092b27bd892d4a935f6
dpa313 <dpa.github@gmail.com>
2024-04-27 15:41:02 +0600

    news_&_category_data

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
 
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-const categories = require('./data/categories.json')
+app.get("/", (req, res) => {
+  res.send("News server");
+});
 
-app.get('/', (req,res) =>{
-    res.send("News server")
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get('/news', (req,res)=>{
+    res.send(news)
 })
 
-app.get('/news-categories', (req,res) =>{
-    res.send(categories)
-})
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+  console.log(req.params.id);
+});
 
-app.listen(port, ()=>{
-    console.log(`Server running on ${port}`);
-})
+app.get("/category/:id", (req, res) => {
+  const categoryId = req.params.id;
+  if (categoryId === "0") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === categoryId);
+    res.send(categoryNews);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
