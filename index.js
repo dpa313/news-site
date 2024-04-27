8419d336d29e2d9bb0ab227e9b7c7bbcd92d3422
dpa313 <dpa.github@gmail.com>
2024-04-27 09:25:22 +0600

    backend-basic-news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(cors())
+
+
+const categories = require('./data/categories.json')
+
+app.get('/', (req,res) =>{
+    res.send("News server")
+})
+
+app.get('/news-categories', (req,res) =>{
+    res.send(categories)
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on ${port}`);
+})
